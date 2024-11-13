f76dcd71a6011b5485ae97a8c1d32cf1d01d7032
singhalisha829 <singhalisha829@gmail.com>
2024-11-13 12:07:19 +0530

    build error removed

--- components/NotesList.js
+++ components/NotesList.js
@@ -1,16 +1,22 @@
 import Notes from "./Notes";
-import AddNote from './AddNote';
-import styles from '../styles/Home.module.css';
+import AddNote from "./AddNote";
+import styles from "../styles/Home.module.css";
 
-const NotesList = ({notes, handleAddNote, handleDeleteNote}) =>{
-    return(
-        <div className={styles.notes_list}>
-            {notes.map((note)=>(
-                <Notes id={note.id} text={note.text} date={note.date} handleDeleteNote={handleDeleteNote}/>
-            ))}
-            <AddNote handleAddNote={handleAddNote}/>
-        </div>
-    )
-}
+const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
+  return (
+    <div className={styles.notes_list}>
+      {notes.map((note) => (
+        <Notes
+          key={note.id}
+          id={note.id}
+          text={note.text}
+          date={note.date}
+          handleDeleteNote={handleDeleteNote}
+        />
+      ))}
+      <AddNote handleAddNote={handleAddNote} />
+    </div>
+  );
+};
 
 export default NotesList;
